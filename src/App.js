d95006130634cbcbfaa7df25c6b34e076f046102
kamolpop <kamolpop@KAMOLPOP-s-MacBook-Air.local>
2018-05-11 10:24:14 +0700

    change score

--- src/App.js
+++ src/App.js
@@ -7,38 +7,59 @@ import './App.css';
 
 class App extends Component {
 
+  client_id = '7e996dd32645cda65fd7'
+  client_secret = '0965b089b95d0aba2e8bbebc019c39424355e6c6'
+
   state = {
     scoreTeam: {},
     team: [
       {
         name: 'BorBok',
-        git: ['https://api.github.com/repos/IroncodeCS/daily-report']
-      },
-      {
-        name: 'ตู่และผองเพื่อน',
-        git: [
-              'https://api.github.com/repos/ai2mii/line-hackathon'
-             ]
+        git: ['https://api.github.com/repos/IroncodeCS/daily-report'],
+        score: [0,0,0,0,0,0,0,0,0,0,0,0,0]
       },
       {
         name: 'โคตรเอาเยดโด้วโคตรคูล',
-        git: ['https://api.github.com/repos/banyawat/slackbot']
+        git: ['https://api.github.com/repos/banyawat/slackbot'],
+        score: [0,0,0,0,0,0,0,0,0,0,0,0,0]
       },
       {
         name: 'Fascist',
-        git: ['https://api.github.com/repos/ypsuttiluk/daily-report-hackaton']
+        git: ['https://api.github.com/repos/ypsuttiluk/daily-report-hackaton'],
+        score: [0,0,0,0,0,0,0,0,0,0,0,0,0]
       }
     ]
   }
 
-  
+  getParameterByName(name) {
+    let url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
 
   componentDidMount() {
+
+
+    // 
+    // let github_code = this.getParameterByName('code')
+    // if (github_code) {
+    //   console.log(github_code)
+      // axios.post(`https://github.com/login/oauth/access_token?client_id=${this.client_id}&client_secret=${this.client_secret}&code=${github_code}`)
+      // .then((res)=>{
+      //   console.log(res)
+      // })
+    // }
+    // 
+
     let {scoreTeam} = this.state.scoreTeam
     if (!scoreTeam) {
       scoreTeam = {}
     }
-    let access_token = '3e4c309f4e251a6c49171fa20ce43e64ebabb954'
+    let access_token = 'eeb51d8498d7729e76002ecf33c36898e5e51043'
 
     this.state.team.map((member)=>{
       member.git.map((repo)=>{
@@ -76,7 +97,7 @@ class App extends Component {
 
         {this.state.team.map((childTeam, index)=>{
           return (
-            <ScoreBox key={index} team={childTeam.name} lastUpdate={`${this.state.scoreTeam[childTeam.name]}`}/>
+            <ScoreBox repo={childTeam.git[0]} point={childTeam.score} key={index} team={childTeam.name} lastUpdate={`${this.state.scoreTeam[childTeam.name]}`}/>
           )
         })}
       </div>
--- src/ScoreBox.js
+++ src/ScoreBox.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class ScoreBox extends Component {
+
+  state = {
+    point: 0
+  }
+
+  componentWillReceiveProps() {
+    let point = 0
+    this.props.point.map((p) => {
+      point += p
+      console.log (point)
+    })
+    this.setState({point})
+  }
+
   render() {
     return (
       <div className="score-box">
@@ -9,6 +23,9 @@ class ScoreBox extends Component {
         <div>
           <p className="last-update">Last Updated</p>
           <p className="time-updated">{this.props.lastUpdate}</p>
+          <p className="last-update">Point</p>          
+          <p className="score-text">{this.state.point}</p>     
+          {/* <p><a target="_blank" href={this.props.repo.replace(/api.|repos\//g,'')}>Repo</a></p>      */}
         </div>
       </div>
     );
